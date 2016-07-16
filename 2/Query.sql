544ce9f597daa1acbffaa672439f6f6b8eb28d45
SAKorney <mylifeandlove@bk.ru>
2016-07-16 09:35:06 +0300

    Update Query.sql
    Перепечатал строчки на кириллице из-за проблем кодировки.

--- 2/Query.sql
+++ 2/Query.sql
@@ -1,9 +1,9 @@
 SELECT Customers.CustomerId 
   FROM Customers INNER JOIN Purchases
   ON Customers.CustomerId = Purchases.CustomerId
-  WHERE Purchases.ProductName = 'Молоко' AND
+  WHERE Purchases.ProductName = 'РњРѕР»РѕРєРѕ' AND
         Customers.CustomerId NOT IN ( SELECT DISTINCT CustomerId
                                       FROM Purchases
-                                      WHERE Purchases.ProductName = 'Сметана' )
+                                      WHERE Purchases.ProductName = 'РЎРјРµС‚Р°РЅР°' )
 
 
