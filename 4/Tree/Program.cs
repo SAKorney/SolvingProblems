a43c007ab860cdaad6d2a96bf9ef16900658c082
sakorney <sakorneev90@gmail.com>
2018-01-05 20:30:18 +0300

    Added a solving of tree travels

---
+++ 4/Tree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> root = new Tree<int>(0);
+            Tree<int> l0 = new Tree<int>(1);
+            Tree<int> r0 = new Tree<int>(2);
+            root.Left = l0;
+            root.Right = r0;
+
+            Tree<int> l1 = new Tree<int>(3);
+            Tree<int> r1 = new Tree<int>(4);
+            Tree<int> l2 = new Tree<int>(5);            
+            l0.Right = r1;
+            l0.Left = l1;
+            r0.Left = l2;
+
+            foreach (var item in Tree<int>.TreeTraversal(root, Tree<int>.InOrdered))
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4/Tree/Tree.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithTree
+{
+    class Tree<T>
+    {
+        public Tree<T> Left { get; set; }
+        public Tree<T> Right { get; set; }
+        public T Value { get; set; }
+
+        public Tree(T value)
+        {
+            Value = value;
+        }        
+
+        public static IEnumerable<T> TreeTraversal(Tree<T> root, Func<Tree<T>, IEnumerable<T>> algorithm)
+        {
+            return algorithm(root);
+        }
+
+        public static IEnumerable<T> PreOrdered(Tree<T> root)
+        {
+            yield return root.Value;
+
+            if (root.Left != null)
+            {
+                foreach (var value in PreOrdered(root.Left))
+                {
+                    yield return value;
+                }
+            }
+
+            if (root.Right != null)
+            {
+                foreach (var value in PreOrdered(root.Right))
+                {
+                    yield return value;
+                }
+            }
+        }
+        public static IEnumerable<T> InOrdered(Tree<T> root)
+        {
+            if (root.Left != null)
+            {
+                foreach (var value in InOrdered(root.Left))
+                {
+                    yield return value;
+                }
+            }
+
+            yield return root.Value;
+
+            if (root.Right != null)
+            {
+                foreach (var value in InOrdered(root.Right))
+                {
+                    yield return value;
+                }
+            }
+        }
+        public static IEnumerable<T> PostOrdered(Tree<T> root)
+        {
+            if (root.Left != null)
+            {
+                foreach (var value in PostOrdered(root.Left))
+                {
+                    yield return value;
+                }
+            }
+
+            if (root.Right != null)
+            {
+                foreach (var value in PostOrdered(root.Right))
+                {
+                    yield return value;
+                }
+            }
+
+            yield return root.Value;
+        }
+    }
+}
